e1e2b6a7f1116fdc7d7b0cb72d622c573e83dce3
Carlos Muñoz <carlosmunyozguimera@gmail.com>
2019-07-31 18:40:41 +0200

    view artis and album info from playlist

--- routes/playlistSongCollection.js
+++ routes/playlistSongCollection.js
@@ -17,13 +17,15 @@ router.get('/:id', async (req, res, next) => {
       const superTracks = album.body.albums[0];
       superTracks.tracks.items.forEach((item) => {
         if (item.id === trackIdFromPlaylist) {
+          item.albumId = track.albumId;
           trackInfo.push(item);
+          // console.log(trackInfo);
         }
       });
     });
     const newData = await spotifyApi.getAlbums([playlist.tracks[0].albumId]);
     const album = newData.body;
-    console.log(newData.body);
+    // console.log(newData.body);
     const superInfoTrack = { playlist, album, trackInfo };
     res.render('playlistSongCollection', superInfoTrack);
   } catch (error) {
