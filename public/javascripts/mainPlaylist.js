aee5cc8fe2710182de3aa51eb494ee0af68769a0
Alvaro Zajara <alzca1@gmail.com>
2019-08-01 10:16:07 +0200

    Fixed delete button in playlistCollection

--- public/javascripts/mainPlaylist.js
+++ public/javascripts/mainPlaylist.js
@@ -6,19 +6,19 @@ const main = () => {
   // show playlist
   const showPlaylist = () => {
     const selectPlaylist = document.querySelectorAll('article button');
-    selectPlaylist.forEach((button) => {
-      button.addEventListener('click', async (event) => {
-        console.log(event);
-        try {
-          const id = event.target.id;
-          console.log(id);
-          await axios.post(`/playlistCollection/${id}`);
-          // await axios.post(`/api/recipes/${id}/delete`);
-        } catch (error) {
-          console.log(error);
-        }
-      });
-    });
+    // selectPlaylist.forEach((button) => {
+    //   button.addEventListener('click', async (event) => {
+    //     console.log(event);
+    //     try {
+    //       const id = event.target.id;
+    //       console.log(id);
+    //       await axios.post(`/playlistCollection/${id}`);
+    //       // await axios.post(`/api/recipes/${id}/delete`);
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //   });
+    // });
   };
   showPlaylist();
 
@@ -44,18 +44,34 @@ const main = () => {
       }, 3000);
     } else {
       const article = document.createElement('article');
-      const buttonCreate = document.createElement('button');
-      const buttonDelete = document.createElement('button');
-      buttonCreate.setAttribute('id', newPlaylist._id);
-      buttonDelete.setAttribute('id', newPlaylist._id);
-      buttonCreate.innerText = 'go';
-      buttonCreate.innerText = 'delete';
-      const p = document.createElement('p');
-      p.innerText = `${newPlaylist.name}`;
-      article.appendChild(p);
-      article.appendChild(buttonCreate);
-      article.appendChild(buttonDelete);
+      const formDelete = `
+        <p>${newPlaylist.name}</p>
+        <button class='buttonDelete' id=${newPlaylist._id}>detele</button>
+        <button class='buttonCreate'>create</button>
+      `;
+      article.innerHTML = formDelete;
       playlistCollectionScrolldown.appendChild(article);
+
+      const buttonDelete = document.querySelector('.buttonDelete');
+
+      // const buttonCreate = document.createElement('button');
+      // const buttonDelete = document.createElement('button');
+      // buttonCreate.setAttribute('id', newPlaylist._id);
+      // buttonDelete.setAttribute('id', newPlaylist._id);
+      // buttonCreate.innerText = 'go';
+      // buttonCreate.innerText = 'delete';
+      // const p = document.createElement('p');
+      // p.innerText = `${newPlaylist.name}`;
+      // article.appendChild(p);
+      // article.appendChild(buttonCreate);
+      // article.appendChild(buttonDelete);
+      // playlistCollectionScrolldown.appendChild(article);
+
+      buttonDelete.addEventListener('click', async (e) => {
+        
+        await axios.post(`/api/playlistCollection/${newPlaylist._id}/delete`);
+        article.remove();
+      });
     }
 
     showPlaylist();
